831608583a57c8c975172acf21ac99a8e1d26d29
abedk21 <43498775+abedk21@users.noreply.github.com>
2022-02-25 01:33:23 -0500

    code

---
+++ src/code/Product.java
@@ -0,0 +1,23 @@
+package code;
+
+public class Product {
+	
+	public int id;
+	public String name;
+	public float price;
+	public int count;
+	public String description;
+	public String category;
+	public float averageRating;
+
+	public Product(int id, String name, float price, int count, String description, String category, float averageRating) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.count = count;
+		this.description = description;
+		this.category = category;
+		this.averageRating = averageRating;
+	}
+}
