509536d25e13b3799b7a728f0ee0853eab562797
abedk21 <43498775+abedk21@users.noreply.github.com>
2022-02-25 00:02:12 -0500

    code

---
+++ src/code/User.java
@@ -0,0 +1,33 @@
+package code;
+
+
+public abstract class User {
+	public int id;
+	public String firstName;
+	public String lastName;
+	public String email;
+	public int phoneNumber;
+	public String username;
+	private String password;
+
+	/**
+	 * @param id
+	 * @param firstName
+	 * @param lastName
+	 * @param email
+	 * @param phoneNumber
+	 * @param username
+	 * @param password
+	 */
+	public User(int id, String firstName, String lastName, String email, int phoneNumber, String username, String password) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+		this.username = username;
+		this.password = password;
+	}
+}
+
